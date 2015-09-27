ea3b27751cc8e800005372206d9ab52daf123332
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-27 07:07:05 +0530

    added 'home' and 'timeline' states

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ola', ['ionic', 'ngCordova', 'ola.controllers', 'ola.services'])
+angular.module('ola', ['ionic','ngSanitize', 'ngCordova', 'angular-timeline', 'ionic.contrib.ui.cards', 'ola.controllers', 'ola.services'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -30,6 +30,14 @@ angular.module('ola', ['ionic', 'ngCordova', 'ola.controllers', 'ola.services'])
             templateUrl: 'templates/menu.html',
             controller: 'AppCtrl'
         })
+        .state('app.home', {
+            url: '/home',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/home.html'
+                }
+            }
+        })
         .state('app.places', {
             url: '/places',
             views: {
@@ -38,7 +46,16 @@ angular.module('ola', ['ionic', 'ngCordova', 'ola.controllers', 'ola.services'])
                     controller: 'PlacesCtrl'
                 }
             }
-        });
-    // if none of the above states are matched, use this as the fallback
+        })
+        .state('app.timeline', {
+            url: '/timeline',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/timeline.html',
+                    controller: 'TimelineCtrl'
+                }
+            }
+        })
+        // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/places');
 });
