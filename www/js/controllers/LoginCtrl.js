788eea9991bdf785c452e1e2d939a829f32c82af
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-27 07:06:12 +0530

    - added support for 'book_cab' - added support for 'social user context' to fetch personalized location recommendations

--- www/js/controllers/LoginCtrl.js
+++
@@ -1,9 +0,0 @@
-function LoginCtrl($cordovaOauth) {
-    $scope.facebookLogin = function() {
-        $cordovaOauth.facebook("CLIENT_ID_HERE", ["email"]).then(function(result) {
-            // results
-        }, function(error) {
-            // error
-        });
-    }
-}
--- www/js/services/ApiService.js
+++ www/js/services/ApiService.js
@@ -1,32 +1,49 @@
-function ApiService($http, $cordovaOauth) {
+function ApiService($rootScope, $http, $cordovaOauth, $localStorage) {
     var BASE_URL = "http://localhost:8100";
+    // var BASE_URL = "http://test-387dcco7.cloudapp.net/v1/ola";
     var FACEBOOK_CLIENT_ID = 1608052562789205;
+    var FACEBOOK_GRAPH_API_URL = "https://graph.facebook.com/v2.4";
 
     var a = {
         facebookLogin: facebookLogin,
+        getUserInfo: getUserInfo,
         logout: logout,
-        getPlaces: getPlaces
+        getPlaces: getPlaces,
+        bookCab: bookCab
     }
     return a;
 
     function facebookLogin() {
-        $cordovaOauth.facebook(FACEBOOK_CLIENT_ID, ["user_birthday", "user_friends", "user_about_me", "user_status", "user_posts", "email", "public_profile"]).then(function(result) {
-            // results
-            console.log('yay');
-            console.log(result);
-        }, function(error) {
-            // error
-            alert('damn nigga');
+        var accessTokenScope = ["user_birthday", "user_friends", "user_about_me", "user_status", "user_posts", "email", "public_profile"];
+        return $cordovaOauth.facebook(FACEBOOK_CLIENT_ID, accessTokenScope);
+    }
+
+    function getUserInfo(access_token) {
+        var endpointUrl = FACEBOOK_GRAPH_API_URL + "/me";
+        var userInfoParams = {
+            fields: "id,name,posts.limit(200){message},about,bio,picture,cover",
+            access_token: access_token
+        }
+        return $http.get(endpointUrl, {
+            params: userInfoParams
         });
-        // console.log('logged in');
     }
 
     function logout() {
         console.log('logged out');
     }
 
-    function getPlaces(geolocation) {
-        return $http.get(BASE_URL + "/get_locations", {
+    function getPlaces(geolocation, userInfo) {
+        var postData = {
+            latitude: geolocation.latitude,
+            longitude: geolocation.longitude,
+            text: userInfo
+        };
+        return $http.post(BASE_URL + "/get_locations", postData);
+    }
+
+    function bookCab(geolocation) {
+        return $http.get(BASE_URL + "/book_cab", {
             params: geolocation
         });
     }
