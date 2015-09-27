c838779d5a379f45247675cb4092d8f92287ef57
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-27 07:08:12 +0530

    added PlacesCtrl and TimelineCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('ola.controllers', ['ola.services'])
-    .controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', AppCtrl])
-    .controller('PlacesCtrl', ['$scope', '$ionicLoading', '$q', '$timeout', 'apiService', PlacesCtrl]);
-    // .controller('LoginCtrl',['$scope','$ionicLoading','$timeout','apiService', LoginCtrl]);
+angular.module('ola.controllers', ['ola.services', 'ola.utils'])
+    .controller('AppCtrl', ['$rootScope', '$scope', '$ionicModal', '$timeout', '$localStorage', 'apiService', 'userService', AppCtrl])
+    .controller('PlacesCtrl', ['$rootScope', '$scope', '$ionicLoading', '$ionicPlatform', '$q', '$timeout', '$cordovaGeolocation', 'apiService', 'userService', '$state', PlacesCtrl])
+    .controller('TimelineCtrl', ['$rootScope', '$scope', '$q', '$timeout', 'apiService', 'userService', TimelineCtrl]);
