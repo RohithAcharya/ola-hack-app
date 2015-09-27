57d89855ba7d9e67c4e2abb02010229b3575ef90
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-27 16:01:42 +0530

    updated 'ola.controllers' module - added explore and explore-timeline

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,7 @@
 angular.module('ola.controllers', ['ola.services', 'ola.utils'])
     .controller('AppCtrl', ['$rootScope', '$scope', '$ionicModal', '$timeout', '$localStorage', 'apiService', 'userService', AppCtrl])
     .controller('PlacesCtrl', ['$rootScope', '$scope', '$ionicLoading', '$ionicPlatform', '$q', '$timeout', '$cordovaGeolocation', 'apiService', 'userService', '$state', PlacesCtrl])
-    .controller('TimelineCtrl', ['$rootScope', '$scope', '$q', '$timeout', 'apiService', 'userService', TimelineCtrl]);
+    .controller('TimelineCtrl', ['$rootScope', '$scope', '$q', '$timeout', '$ionicPlatform', '$cordovaCamera', 'apiService', 'userService', '$ionicScrollDelegate', TimelineCtrl])
+    .controller('ExploreCtrl', ['$rootScope', '$scope', 'apiService', 'userService', '$state', '$ionicLoading', ExploreCtrl])
+    .controller('ProfileCtrl', ['$rootScope', '$scope', '$timeout', ProfileCtrl])
+    .controller('ExploreTimelineCtrl', ['$scope', 'userService', 'apiService', '$timeout', '$ionicScrollDelegate', '$ionicPlatform', '$cordovaCamera', '$ionicLoading', ExploreTimelineCtrl]);
